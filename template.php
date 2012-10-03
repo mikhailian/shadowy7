f4c1cef6866805f972606a19f29f2fc564cc5697
Alexander Mikhailian <mikhailian@mova.org>
2012-10-03 23:25:03 +0200

    Finished the design of the homepage.

---
+++ template.php
@@ -0,0 +1,11 @@
+<?php
+// First, we must set up an array
+/*$element = array(
+  '#tag' => 'link', // The #tag is the html tag - <link />
+  '#attributes' => array( // Set up an array of attributes inside the tag
+    'href' => 'http://fonts.googleapis.com/css?family=Ubuntu&subset=latin,cyrillic', 
+    'rel' => 'stylesheet',
+    'type' => 'text/css',
+  ),
+);
+drupal_add_html_head($element, 'google_font_cardo');*/
