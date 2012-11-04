0a9425567315b47a9c35926bd41d4ff0ad5ef4bd
Alexander Mikhailian <mikhailian@mova.org>
2012-11-04 12:23:11 +0100

    Before trying in future production.

--- template.php
+++ template.php
@@ -10,4 +10,39 @@
 );
 drupal_add_html_head($element, 'google_font_cardo');*/
 
-
+function shadowy7_username($variables) {
+  $u_css_class = '';
+  $u = user_load($variables['account']->uid);
+  // style depends on the role
+  if ($u->uid == 1) {
+    $u_css_class='administrator';
+    $u_title=t('Administrator');
+  }
+  elseif (in_array('node moderator', array_values($u->roles))) {
+    $u_css_class='node-moderator';
+    $u_title=t('Node moderator');
+  }
+  elseif (in_array('forum moderator', array_values($u->roles))) {
+    $u_css_class='forum-moderator';
+    $u_title=t('Forum moderator');
+  }
+  else {
+    $u_css_class='ordinary-user';
+    $u_title=t('Ordinary user');
+  }
+  $variables['link_options']['attributes']['class'][] = $u_css_class;
+  $variables['link_options']['attributes']['title'] = $u_title;
+  if (isset($variables['link_path'])) {
+    // We have a link path, so we should generate a link using l().
+    // Additional classes may be added as array elements like
+    // $variables['link_options']['attributes']['class'][] = 'myclass';
+    $output = l($variables['name'] . $variables['extra'], $variables['link_path'], $variables['link_options']);
+  }
+  else {
+    // Modules may have added important attributes so they must be included
+    // in the output. Additional classes may be added as array elements like
+    // $variables['attributes_array']['class'][] = 'myclass';
+    $output = '<span' . drupal_attributes($variables['attributes_array']) . '>' . $variables['name'] . $variables['extra'] . '</span>';
+  }
+  return $output;
+}
